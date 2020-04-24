be5dfd6ba87c3c0413ce0816ea78ee39dcb875e6
ezreal12 <ezreal12@naver.com>
2020-04-25 00:57:48 +0900

    파일 업로드 테스트 페이지 생성

--- src/main/java/com/server/pica/controller/MainCont.java
+++ src/main/java/com/server/pica/controller/MainCont.java
@@ -1,11 +1,22 @@
 package com.server.pica.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+
+
 
 @Controller
 @RequestMapping("/")
 public class MainCont {
+	//C:\Users\SiuKim\Desktop\sample
+	private static final String UPLOAD_PATH = "C:\\Users\\SiuKim\\Desktop\\sample";
 	@RequestMapping("test")
 	public String test() {
 		return "test";
@@ -14,4 +25,38 @@ public class MainCont {
 	public String home() {
 		return "home";
 	}
+	
+	@RequestMapping(value = "/picUpload.do",method = RequestMethod.POST)
+	public void upload(MultipartFile uploadfile){
+	    //logger.info("upload() POST 호출");
+	    //logger.info("파일 이름: {}", uploadfile.getOriginalFilename());
+	    //logger.info("파일 크기: {}", uploadfile.getSize());
+		System.out.println("upload() POST 호출");
+		System.out.println("파일 이름: {"+uploadfile.getOriginalFilename()+"}");
+		System.out.println("파일 크기: {"+ uploadfile.getSize()+"}");
+	    saveFile(uploadfile);
+
+	}
+
+	private String saveFile(MultipartFile file){
+	    // 파일 이름 변경
+	    UUID uuid = UUID.randomUUID();
+	    String saveName = uuid + "_" + file.getOriginalFilename();
+
+	    //logger.info("saveName: {}",saveName);
+	    System.out.println("saveName: {"+saveName+"}");
+
+	    // 저장할 File 객체를 생성(껍데기 파일)ㄴ
+	    File saveFile = new File(UPLOAD_PATH,saveName); // 저장할 폴더 이름, 저장할 파일 이름
+
+	    try {
+	        file.transferTo(saveFile); // 업로드 파일에 saveFile이라는 껍데기 입힘
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return null;
+	    }
+
+	    return saveName;
+	} // end saveFile(
+	
 }
